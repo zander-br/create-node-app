a2cf24b7248d894c00af1ee1a687e6240b979059
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-03-16 10:14:35 -0300

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const inquirer = require('inquirer');
+const path = require('path');
+const clone = require('git-clone');
+const shell = require('shelljs');
+
+const QUESTIONS = [
+  {
+    name: 'name',
+    type: 'input',
+    message: 'Please input a new project name:',
+  },
+]
+
+inquirer.prompt(QUESTIONS).then(answers => {
+  const currentDir = process.cwd();
+  const projectName = answers['name'];
+  const tartgetPath = path.join(currentDir, projectName);
+  const templateRepo = 'https://github.com/zander-br/node-clean-architecture-demo';
+
+  clone(templateRepo, tartgetPath, () => {
+    shell.cd(tartgetPath);
+    shell.exec('yarn');
+  });
+});
